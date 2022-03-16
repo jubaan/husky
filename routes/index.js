bfcf7cd21caf8dab90ed99fd4df200772a4dd6a3
Julio Añoveros <julio.ab@outlook.com>
2022-03-16 10:00:37 -0600

    Add signup and login views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/models', function (req, res, next) {
+  let modelId = req.params.id;
+
+  res.render(`models`, { title: 'Express' });
+});
+
+router.get('/model', function (req, res, next) {
+  let modelId = req.params.id;
+
+  res.render(`modelDetails`, { title: 'Express' });
+});
+
+router.get('/cart', function (req, res, next) {
+  let modelId = req.params.id;
+
+  res.render(`cart`, { title: 'Express' });
+});
+
+router.get('/login', function (req, res, next) {
+  let modelId = req.params.id;
+
+  res.render(`login`, { title: 'Express' });
+});
+
+router.get('/signup', function (req, res, next) {
+  let modelId = req.params.id;
+
+  res.render(`signup`, { title: 'Express' });
 });
 
 module.exports = router;
