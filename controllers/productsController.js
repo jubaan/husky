c507d9c0c520eb1b6198c3eb507724e54cdb4d4c
Julio Añoveros <julio.ab@outlook.com>
2022-03-20 14:28:08 -0600

    Add CRUD for models

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const productsController = {
   index: (req, res) => {
     res.render('products/models', { title: 'Models' });
@@ -8,14 +10,26 @@ const productsController = {
     res.render('products/modelDetails', { title: 'Express' });
   },
   create: (req, res) => {
+    // let models = JSON.parse(fs.readFyleSync('models.json', 'utf-8'));
+
     res.render('products/create', { title: 'Create' });
+  },
+  save: (req, res) => {
+    let models = JSON.parse(fs.readFileSync('models.json', 'utf-8'));
+    let model = JSON.stringify(req.body);
+ 
+    fs.appendFileSync('models.json', model);
+    res.redirect('/models');
   },
   update: (req, res) => {
     res.render('products/edit', { title: 'Update' });
   },
-  delete: (req, res) => {},
+  edit: (req, res) => {},
+  update: (req, res) => {},
   delete: (req, res) => {
-    res.render('models', { title: 'Express' });
+    let modelId = req.params.id;
+
+    res.redirect('/models', { title: 'Express' });
   },
 };
 
--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 const productsController = require('../controllers/productsController');
 
+
 router.get('/', productsController.index);
 router.get('/create', productsController.create);
+router.post('/save', productsController.save);
 router.get('/:id', productsController.show);
+router.get('/:id/edit', productsController.edit);
+router.patch('/:id/edit', productsController.update);
 router.delete('/:id/delete', productsController.delete);
 
 module.exports = router;
