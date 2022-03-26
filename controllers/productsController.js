16a3c7b119feb49a9ef036cf410c76bdaeddb4ca
Julio Añoveros <julio.ab@outlook.com>
2022-03-26 17:52:30 -0600

    Add new and edit views for products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,30 +1,100 @@
 const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+let modelsPath = path.join(__dirname, '../models.json');
+let modelsFile = fs.readFileSync(modelsPath, { encoding: 'utf-8' });
+let models;
 
 const productsController = {
   index: (req, res) => {
-    res.render('products/models', { title: 'Models' });
+    models = JSON.parse(modelsFile);
+
+    res.render('products/models', { title: 'Models', models: models });
   },
   show: (req, res) => {
     let modelId = req.params.id;
 
-    res.render('products/modelDetails', { title: 'Express' });
+    models = JSON.parse(modelsFile);
+
+    let model = models.find((model) => model.id === modelId);
+
+    res.render('products/modelDetails', { title: model.name, model: model  });
   },
   create: (req, res) => {
-    // let models = JSON.parse(fs.readFyleSync('models.json', 'utf-8'));
-
     res.render('products/create', { title: 'Create' });
   },
   save: (req, res) => {
-    let models = JSON.parse(fs.readFileSync('models.json', 'utf-8'));
-    let model = JSON.stringify(req.body);
- 
-    fs.appendFileSync('models.json', model);
-    res.redirect('/models');
+    if (modelsFile === '') {
+      models = [];
+    } else {
+      models = JSON.parse(modelsFile);
+    }
+
+    let model = {
+      id: uuidv4(),
+      name: req.body.name,
+      description: req.body.description,
+      model: req.body.model,
+      type: req.body.type,
+      imageFile: req.body.file,
+      price: req.body.price,
+    };
+
+    models.push(model);
+    modelsJSON = JSON.stringify(models);
+
+    fs.writeFileSync(modelsPath, modelsJSON);
+    res.redirect('/models')
   },
   update: (req, res) => {
-    res.render('products/edit', { title: 'Update' });
+    let modelId = req.params.id;
+
+    if (modelsFile === '') {
+      models = [];
+    } else {
+      models = JSON.parse(modelsFile);
+    }
+
+    let model = models.find((model) => model.id === modelId);
+
+    model = {
+      id: uuidv4(),
+      name: req.body.name,
+      description: req.body.description,
+      model: req.body.model,
+      type: req.body.type,
+      price: req.body.price,
+    };
+
+    models.push(model);
+    modelsJSON = JSON.stringify(models);
+
+    fs.writeFileSync(modelsPath, modelsJSON);
+
+    res.render('products/edit', { title: 'Edit', model: model });
   },
-  edit: (req, res) => {},
+  edit: (req, res) => {
+    let modelId = req.params.id;
+    
+    let types = [
+      'electric',
+      'enduro',
+      'travel',
+      'naked',
+      'supermoto',
+    ];
+
+     if (modelsFile === '') {
+       models = [];
+     } else {
+       models = JSON.parse(modelsFile);
+     }
+
+    let model = models.find((model) => model.id === modelId);
+    
+    res.render('products/edit', { title: 'Edit', model: model, types: types });
+  },
   update: (req, res) => {},
   delete: (req, res) => {
     let modelId = req.params.id;
